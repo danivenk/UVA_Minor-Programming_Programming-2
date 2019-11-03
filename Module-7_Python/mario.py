156ac499799f698b995a97bf39a186af7578c9a1
danivenk <danivenk@gmail.com>
2019-11-03 16:45:26 +0100

    max 24 height

--- Module-7_Python/mario.py
+++ Module-7_Python/mario.py
@@ -28,7 +28,7 @@ def get_input(user_promt):
             value = -110
 
         # if value is positive break
-        if value > 0:
+        if 0 < value < 25:
             break
 
     return value
