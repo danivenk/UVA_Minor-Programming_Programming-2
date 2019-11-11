7edced0c2524edd86c9ebc53951c7320e7a30881
danivenk <danivenk@gmail.com>
2019-11-11 23:17:17 +0100

    debug such that check50 gives only green

--- Module-8_Hangman/Ex1-game_of_cards/cardgame.py
+++ Module-8_Hangman/Ex1-game_of_cards/cardgame.py
@@ -9,15 +9,17 @@ Dani van Enk, 11823526
 # import library
 import random
 
+
 class Card:
     """
     Card class defines a card with suit and value
-    
+
     methods:
     description() - returns description of the Card class;
     getValue() - gets Card value;
     getSuit() - gets Card suit
     """
+
     def __init__(self, value, suit):
         """
         initializes the Card class
@@ -26,8 +28,8 @@ class Card:
         value - value of the card as a string;
         suit - suit of the card as a string
         """
-        self._value = value
-        self._suit = suit
+        self.value = value
+        self.suit = suit
 
     def description(self):
         """
@@ -35,7 +37,7 @@ class Card:
 
         returns readable string
         """
-        return f"{self._value} of {self._suit}"
+        return f"{self.value} of {self.suit}"
 
     def getValue(self):
         """
@@ -43,7 +45,7 @@ class Card:
 
         returns Card value
         """
-        return self._value
+        return self.value
 
     def getSuit(self):
         """
@@ -51,30 +53,31 @@ class Card:
 
         returns Card suit
         """
-        return self._suit
+        return self.suit
 
 
 class Deck:
     """
     defines a deck of 52 cards
-    
+
     methods:
     description() - returns description of the Deck class;
     shuffle() - shuffel the deck of cards;
     deal() - pick one random card from the deck
     """
+
     def __init__(self):
         """
         initializes the Deck class
         """
-        self._values = ["Two", "Three", "Four", "Five", "Six", "Seven", \
-                        "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
+        self._values = ['A', '2', '3', '4', '5', '6', '7', '8', '9',
+                        '10', 'J', 'Q', 'K']
         self._suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-        self.cards = []
+        self._cards = []
 
         for suit in self._suits:
             for value in self._values:
-                self.cards.append(Card(value, suit))
+                self._cards.append(Card(value, suit))
 
     def description(self):
         """
@@ -82,13 +85,13 @@ class Deck:
 
         returns readable string
         """
-        return f"There are {len(self.cards)} cards in the deck"
+        return f"There are {len(self._cards)} cards in the deck"
 
     def shuffle(self):
         """
         shuffles the deck of cards
         """
-        random.shuffle(self.cards)
+        random.shuffle(self._cards)
 
     def deal(self):
         """
@@ -96,7 +99,7 @@ class Deck:
 
         returns a card
         """
-        return self.cards.pop(0)
+        return self._cards.pop()
 
 
 def main():
@@ -104,6 +107,7 @@ def main():
     runs used functions
     """
     dealRandomCard()
+
 
 def dealRandomCard():
     """
@@ -117,9 +121,9 @@ def dealRandomCard():
 
     # shuffles the deck
     deck.shuffle()
-    
+
     # deal a card
-    print(deck.deal())
+    print(deck.deal().description())
 
     # print current state of the deck
     print(deck.description())
