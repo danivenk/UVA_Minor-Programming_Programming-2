6467d99293cc133b186328cd71ead25f9125200a
danivenk <danivenk@gmail.com>
2019-11-18 15:26:13 +0100

    try comibining the assertions in the guess funct

--- Module-8_Hangman/Ex3-hangman/hangman.py
+++ Module-8_Hangman/Ex3-hangman/hangman.py
@@ -54,8 +54,7 @@ class Hangman:
 	def guess(self, letter):
 		# Update the game for a guess of letter. Return True if the letter
 		# is added to the pattern, return False if it is not.
-		assert letter.isalpha()
-		assert len(letter) == 1
+		assert letter.isalpha() and len(letter) == 1
 		if letter in self.guessed_letters:
 			return True
 
