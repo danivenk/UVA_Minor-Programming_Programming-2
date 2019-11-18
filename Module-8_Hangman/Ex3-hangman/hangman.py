90172626e6d60cc1be647fdc6955e03cd4748d32
danivenk <danivenk@gmail.com>
2019-11-18 13:45:14 +0100

    Letter already in pattern returns false

--- Module-8_Hangman/Ex3-hangman/hangman.py
+++ Module-8_Hangman/Ex3-hangman/hangman.py
@@ -59,7 +59,7 @@ class Hangman:
 		assert letter.isalpha()
 		assert len(letter) == 1
 		if letter in self.guessed_letters:
-			return True
+			return False
 
 		possible_combinations = []
 		self.guesses += 1
