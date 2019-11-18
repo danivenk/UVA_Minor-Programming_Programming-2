51679000b5a107b4a5d8a1afca8e7e75b2dd3d38
danivenk <danivenk@gmail.com>
2019-11-18 13:09:20 +0100

    revert last commit

--- Module-8_Hangman/Ex3-hangman/hangman.py
+++ Module-8_Hangman/Ex3-hangman/hangman.py
@@ -56,8 +56,7 @@ class Hangman:
 		# is added to the pattern, return False if it is not.
 		letter = letter.lower()
 
-		assert letter.isalpha()
-		assert len(letter) == 1
+		assert letter.isalpha() and len(letter) == 1
 		if letter in self.guessed_letters:
 			return True
 
