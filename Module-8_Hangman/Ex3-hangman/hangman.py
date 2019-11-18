a8e69880d4fb3193008878c12a198ec812eb3a88
danivenk <danivenk@gmail.com>
2019-11-18 16:16:13 +0100

    assert letter is not in guess string

--- Module-8_Hangman/Ex3-hangman/hangman.py
+++ Module-8_Hangman/Ex3-hangman/hangman.py
@@ -54,13 +54,9 @@ class Hangman:
 	def guess(self, letter):
 		# Update the game for a guess of letter. Return True if the letter
 		# is added to the pattern, return False if it is not.
-		letter = str(letter)
+		letter = str(letter).lower()
 
-		assert letter.isalpha() and len(letter) == 1
-
-		letter = letter.lower()
-		if letter in self.guessed_letters:
-			return True
+		assert letter.isalpha() and len(letter) == 1 and letter not in self.guessed_letters
 
 		possible_combinations = []
 		self.guesses += 1
