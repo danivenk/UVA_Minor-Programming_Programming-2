b23a225e9ea99b77ead9df3dc0f405d847bd3c68
danivenk <danivenk@gmail.com>
2019-11-20 14:40:07 +0100

    added comments of the Hangman class

--- Module-8_Hangman/Ex3-hangman/hangman.py
+++ Module-8_Hangman/Ex3-hangman/hangman.py
@@ -75,7 +75,8 @@ class Hangman:
 
 	def __init__(self, length, num_guesses):
 		"""
-		initializes a game of hangman
+		initializes a game of evil hangman with words of len length and
+			with maximum number of guesses of num_guesses
 
 		gives an error if length not positive or number of guesses not positive
 
@@ -97,48 +98,82 @@ class Hangman:
 		self.guess_string = length * "_"
 
 	def guess(self, letter):
-		# Update the game for a guess of letter. Return True if the letter
-		# is added to the pattern, return False if it is not.
+		"""
+		guesses a letter and eliminates the words from which it is easy
+			to guess the word for the user and adds the letter corrispondingly
+
+		give an error if letter isn't an alpha, more than 1 letter or
+			already guessed
+
+		parameters:
+		letter - letter to be guessed
+
+		returns whether or not the letter has been added to the pattern
+		"""
+		
+		# make sure letter is a string
 		letter = str(letter).lower()
 
-		assert letter.isalpha() and len(letter) == 1 and letter not in self.guessed_letters
+		# assume letter is 1 character, a letter and not yet guessed
+		assert letter.isalpha() and len(letter) == 1 and \
+			letter not in self.guessed_letters
 
 		possible_combinations = []
 		self.guesses += 1
 
+		# add letter to guessed_letter
 		self.guessed_letters += letter
 
+		# loop over each word 
 		for word in self.words:
 			letter_pos_string = ""
+
+			# loop over each letter
 			for char in range(len(word)):
+
+				# add letter to position if letter not in guess_string for letter
 				if word[char] != self.guess_string[char] and word[char] == letter:
 					letter_pos_string += letter
+
+				# else add guess_string letter
 				else:
 					letter_pos_string += self.guess_string[char]
 
+			# set bool for if word is added
 			word_added = False
 
+			# initialize possible_combinations in correct format if not yet
+			#	initialized else add combination to possible combinations
 			if len(possible_combinations) == 0:
 				possible_combinations.append([letter_pos_string, 1, [word]])
 			else:
+
+				# loop over each item in possible combinations
 				for item in possible_combinations:
+
+					# count words for each possible combinations and save words
 					if letter_pos_string in item:
 						item[1] += 1
 						item[2].append(word)
 						word_added = True
 						break
 
+				# if combination is not yet in possible_combinations add it
 				if not word_added:
 					possible_combinations.append([letter_pos_string, 1, [word]])
 
+		# dummy counter
 		dummy = 0
 
+		# find the possible combination with the most words associated
 		for item in possible_combinations:
+			# if found combination with more words
 			if len(item[2]) > dummy:
 				dummy = len(item[2])
 				self.words = item[2]
 				self.guess_string = item[0]
 
+		# return true if letter is added else false
 		if letter in self.guess_string:
 			return True
 		else:
@@ -146,48 +181,76 @@ class Hangman:
 
 
 	def pattern(self):
-		# Return a string of the current game pattern. Use underscores in
-		# place of missing letters. Example: "_AN_MAN".
+		"""
+		returns current guess_string
+		"""
+
 		return self.guess_string
 
 	def guessed_string(self):
-		# Produce a string of all letters guessed so far, in the order they
-		# were guessed.
+		"""
+		returns all guessed letters in order of guessed
+		"""
+
 		return self.guessed_letters
 
 	def consistent_word(self):
-		# Produce a word that is consistent with the current pattern.
+		"""
+		returns a word consistent with current pattern
+		"""
+		
 		return self.words[0]
 
 	def finished(self):
-		# Return True if the game is finished, otherwise False.
+		"""
+		check if game is finished
+
+		returns true if game if finished else false
+		"""
+		
+		# when guessed more than maximum number of guesses or
+		# 	if all letters are guessed return true else false
 		if self.guesses >= self.num_guesses or self.guesses == ALPHA:
 			return True
 		else:
 			return False
 
 	def won(self):
-		# Return True if the game is finished and the player has won,
-		# otherwise False.
+		"""
+		checks if game is won
+
+		returns true if won else false
+		"""
+		
+		# if game finished and no _ left return true else false
 		if self.finished() and "_" not in self.guess_string:
 			return True
 		else:
 			return False
 
 	def lost(self):
-		# Return True if the game is finished and the player has lost,
-		# otherwise False.
+		"""
+		checks if game is lost
+
+		returns true if lost else false
+		"""
+		
+		# if game finished and _ left return true else false
 		if self.finished() and "_" in self.guess_string:
 			return True
 		else:
 			return False
 
 	def __str__(self):
-		# Return a string representation of the game with some relevant
-		# statistics.
-		class_str = f"letters guessed are {self.guessed_letters}, \
-					{len(self.words)} words remaining, guesses remaining \
-					{self.num_guesses - self.guesses}"
+		"""
+		returns statistics of game
+		"""
+		
+		# statistics of the game, guessed_letters, number of words left
+		#	and guesses left
+		class_str = f"letters guessed are {self.guessed_letters}," + \
+					f"{len(self.words)} words remaining, guesses remaining" + \
+					f"{self.num_guesses - self.guesses}"
 
 		return class_str
 
