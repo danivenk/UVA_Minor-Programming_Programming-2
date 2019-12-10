00106466a9c74851e583e7dc8c8edb29a8a96869
danivenk <danivenk@gmail.com>
2019-12-10 22:55:59 +0000

    application is able to read form

--- Module-10_Web/Ex2-survey/survey/application.py
+++ Module-10_Web/Ex2-survey/survey/application.py
@@ -1,7 +1,7 @@
 import cs50
 import csv
 
-from flask import Flask, jsonify, redirect, render_template, request
+from flask import Flask, abort, jsonify, redirect, render_template, request
 
 # Configure application
 app = Flask(__name__)
@@ -31,7 +31,15 @@ def get_form():
 
 @app.route("/form", methods=["POST"])
 def post_form():
-    return render_template("error.html", message="TODO")
+
+    name = request.form.get("name")
+    mtype = request.form.get("mtype")
+    adult = request.form.get("adult")
+
+    if not name or not mtype or not adult:
+        return render_template("error.html", message="400: not enough input to submit")
+
+    return render_template("error.html", message=f"{name}, {mtype}, 18+ {adult}")
 
 
 @app.route("/sheet", methods=["GET"])
